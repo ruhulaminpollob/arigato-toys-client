d98264a7eed23ed571a0c6b0393182b40dcdc0d0
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-06-06 23:51:09 +0600

    searcch method successfully added to the all toy secction

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -7,7 +7,27 @@ const AllToys = () => {
     const loadedAllToys = useLoaderData()
     const [allToys, setAllToys] = useState(loadedAllToys)
 
-    
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredResults, setFilteredResults] = useState([]);
+
+    //
+    const handleSearchChange = (event) => {
+
+        const query = event.target.value;
+        setSearchQuery(query);
+        if (query.length >2) {
+            
+            const filtered = allToys.filter((item) =>
+                item.name.toLowerCase().includes(query.toLowerCase())
+            );
+            setFilteredResults(filtered);
+        }
+
+
+    };
+
+
+
 
     return (
 
@@ -22,13 +42,35 @@ const AllToys = () => {
                     <input
                         className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                         type="text"
-                        placeholder="Find Your Desire Toys"
+                        // placeholder="Find Your Desire Toys"
+                        placeholder="Search by name"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
                     />
                     <button className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
                         Search
                     </button>
                 </div>
             </div>
+            {
+                filteredResults?.length && <>
+                    <div className='bg-indigo-50 p-3 md:p-10 rounded-xl mt-10'>
+                        <div>
+                            <h1 className='text-2xl font-bold p-3 underline'>Search Results</h1>
+                        </div>
+                        <div className='grid md:grid-cols-2 gap-5  md:gap-10 '>
+                            {
+                                filteredResults.map(singleToy => <AllToysCard key={singleToy._id} singleToy={singleToy}></AllToysCard>)
+                            }
+                        </div>
+
+                    </div>
+                    <div>
+                        <h1 className='text-2xl font-bold p-3 mt-5 underline'>Explore More</h1>
+                    </div>
+                </>
+            }
+
             <div className='grid md:grid-cols-2 gap-5 bg-indigo-50 p-3 md:p-10 rounded-xl md:gap-10 mt-10'>
 
 
